573611dce8f8f510931ded54ff5fb9497980bcb2
sam.lai <sam.lai@silksoftware.com>
2023-04-11 10:53:04 +0800

    一级二级路由cheak

--- djangoLearning/appT/views.py
+++ djangoLearning/appT/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
+def demo(request):
+    print(request,'right here! ')
+    return HttpResponse('<h1>这是返回来滴标签 嘿嘿！</h1>')
 # Create your views here.
+
+def demo2(request):
+    print(request,'right here! ')
+    return HttpResponse('<h1>demo2F</h1>')
